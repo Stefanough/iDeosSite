1696be13b3edf21e23f5572afaca926c545af374
Stefan <stefan.armijo@gmail.com>
2018-09-11 16:37:57 -0700

    correct spelling and import Bio Images

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import victor from '../../assets/bioImages/victor.jpg';
+import stefan from '../../assets/bioImages/stefan.jpg';
+import will from '../../assets/bioImages/will.jpg';
+import james from '../../assets/bioImages/james.jpg';
 
 const Bio = () => {
   return (
@@ -7,25 +11,25 @@ const Bio = () => {
       <h5>Team</h5>
       <p>We believe in making blockchain development accessible and approachable for every developer.</p>
       <div className="Vic">
-        <img id="vic" src="./../../assets/idpic.jpeg"></img>
+        <img id="vic" src={victor}></img>
         <h3>Victor Faucon</h3>
         <SocialIcon url="https://github.com/VernSul"></SocialIcon>
         <SocialIcon url="https://www.linkedin.com/in/victor-faucon-8582a333/" ></SocialIcon>
       </div>
-      <div className="Stephan">
-        <img id="Stephan" src="./../../assets/stephan.jpeg"></img>
-        <h3>Stephan Armijo</h3>
+      <div className="Stefan">
+        <img id="Stefan" src={stefan}></img>
+        <h3>Stefan Armijo</h3>
         <SocialIcon url="https://github.com/Stefanough"></SocialIcon>
         <SocialIcon url="https://www.linkedin.com/in/stefanarmijo/"></SocialIcon>
       </div>
       <div className="Will">
-        <img id="Will" src="./../../assets/will.jpeg"></img>
+        <img id="Will" src={will}></img>
         <h3>Will LeGate</h3>
         <SocialIcon url="https://github.com/wlegate"></SocialIcon>
         <SocialIcon url="https://www.linkedin.com/in/william-legate//"></SocialIcon>
       </div>
       <div className="James">
-        <img id="James" src="./../../assets/james.jpeg"></img>
+        <img id="James" src={james}></img>
         <h3>James Tu</h3>
         <SocialIcon url="https://github.com/milesmeow"></SocialIcon>
         <SocialIcon url="https://www.linkedin.com/in/jamestu2000/"></SocialIcon>
