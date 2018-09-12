1c45f4dee267c57b6a0dd517d27c5c25e7d2b698
VernSul <vicfaucon@gmail.com>
2018-09-11 18:15:18 -0700

    Contact from added

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,44 +1,46 @@
 import React from 'react';
-// import nodemailer from 'nodemailer';
+// var nodemailer = require('nodemailer');
 
 const Connect = () => {
 
-  //     const sendEmail = (dest, text) => {
-  //         // Only needed if you don't have a real mail account for testing
-  // nodemailer.createTestAccount((err, account) => {
-  //     // create reusable transporter object using the default SMTP transport
-  //     let transporter = nodemailer.createTransport({
-  //         host: 'smtp.ethereal.email',
-  //         port: 587,
-  //         secure: false, // true for 465, false for other ports
-  //         auth: {
-  //             user: account.user, // generated ethereal user
-  //             pass: account.pass // generated ethereal password
-  //         }
-  //     });
+//       const sendEmail = (dest, text) => {
 
-  //     // setup email data with unicode symbols
-  //     let mailOptions = {
-  //         from: dest, // sender address
-  //         to: 'vicfaucon@gmail.com', // list of receivers
-  //         subject: 'iDeos info request', // Subject line
-  //         text: text, // plain text body
-  //     };
+//         console.log($('#emailAdress').val())
+//           // Only needed if you don't have a real mail account for testing
+//   nodemailer.createTestAccount((err, account) => {
+//       // create reusable transporter object using the default SMTP transport
+//       let transporter = nodemailer.createTransport({
+//           host: 'smtp.ethereal.email',
+//           port: 587,
+//           secure: false, // true for 465, false for other ports
+//           auth: {
+//               user: account.user, // generated ethereal user
+//               pass: account.pass // generated ethereal password
+//           }
+//       });
 
-  //     // send mail with defined transport object
-  //     transporter.sendMail(mailOptions, (error, info) => {
-  //         if (error) {
-  //             return console.log(error);
-  //         }
-  //         console.log('Message sent: %s', info.messageId);
-  //         // Preview only available when sending through an Ethereal account
-  //         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//       // setup email data with unicode symbols
+//       let mailOptions = {
+//           from: dest, // sender address
+//           to: 'vicfaucon@gmail.com', // list of receivers
+//           subject: 'iDeos info request', // Subject line
+//           text: text, // plain text body
+//       };
 
-  //         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  //         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  //     });
-  // });
-  //     }
+//       // send mail with defined transport object
+//       transporter.sendMail(mailOptions, (error, info) => {
+//           if (error) {
+//               return console.log(error);
+//           }
+//           console.log('Message sent: %s', info.messageId);
+//           // Preview only available when sending through an Ethereal account
+//           console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+//           // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+//           // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+//       });
+//   });
+//       }
 
 
 
@@ -46,9 +48,15 @@ const Connect = () => {
     <div className="Connect">
       <h1>Connect</h1>
       <div id="info">
-        <textarea placeholder="Your Message"></textarea>
-        <input placeholder="Your Email"></input>
-        <button>Send</button>
+      <form action="https://formspree.io/vicfaucon@gmail.com" method="POST">
+        <textarea placeholder="Your Message" type="text" name="name"></textarea>
+        <input placeholder="Your Email" type="email" name="_replyto"></input>
+        <button type="submit" value="Send">Send</button>
+    </form>
+        
+        {/* <textarea id="emailText" placeholder="Your Message"></textarea>
+        <input id="emailAdress" placeholder="Your Email"></input>
+        <button onClick={sendEmail($('#emailAdress').val(), $('#emailText').val())}>Send</button> */}
       </div>
     </div>
   )
