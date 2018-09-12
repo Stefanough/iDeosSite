033289262f148330d851c1826997e37986953ceb
Stefan <stefan.armijo@gmail.com>
2018-09-11 20:53:58 -0700

    shrink iDeos icon

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
 import './App.scss';
-import ideosIcon from './../assets/ideosIcon.png'
+import ideosIcon from './../assets/iDeosiconSmall.png'
 
 class App extends React.Component {
   render() {
